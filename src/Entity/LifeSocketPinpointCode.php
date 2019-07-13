393e998565fc2a8fa1d580552d0255ad95fbe3a4
takashikitamura <monkeydolce@gmail.com>
2019-07-13 16:43:47 +0900

    Fix:Entityファイルの修正
    ピンポイントコードがcreate出来なかったので、作り直した

--- src/Entity/LifeSocketPinpointCode.php
+++ src/Entity/LifeSocketPinpointCode.php
@@ -1,102 +1,193 @@
 <?php
 
-
+namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * LifeSocketPinpointCode
- *
- * @ORM\Table(name="life_socket_pinpoint_code")
- * @ORM\Entity
+ * @ORM\Entity(repositoryClass="App\Repository\LifeSocketPinpointCodeRepository")
  */
 class LifeSocketPinpointCode
 {
     /**
-     * @var int
-     *
-     * @ORM\Column(name="id", type="integer", nullable=false)
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="IDENTITY")
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
      */
     private $id;
 
     /**
-     * @var int
-     *
-     * @ORM\Column(name="code", type="integer", nullable=false)
+     * @ORM\Column(type="string", length=255)
      */
-    private $code;
+    private $pinpoint_code;
 
     /**
-     * @var string
-     *
-     * @ORM\Column(name="pinpoint_name", type="string", length=255, nullable=false)
+     * @ORM\Column(type="string", length=255)
      */
-    private $pinpointName;
+    private $pinpoint_name;
 
     /**
-     * @var string
-     *
-     * @ORM\Column(name="pinpoint_name_kana", type="string", length=255, nullable=false)
+     * @ORM\Column(type="string", length=255)
      */
-    private $pinpointNameKana;
+    private $pinpoint_name_kana;
 
     /**
-     * @var string
-     *
-     * @ORM\Column(name="pref_code", type="string", length=255, nullable=false)
+     * @ORM\Column(type="string", length=255)
      */
-    private $prefCode;
+    private $pref_code;
 
     /**
-     * @var string
-     *
-     * @ORM\Column(name="pref_name", type="string", length=255, nullable=false)
+     * @ORM\Column(type="string", length=255)
      */
-    private $prefName;
+    private $pref_name;
 
     /**
-     * @var string
-     *
-     * @ORM\Column(name="pref_name_kana", type="string", length=255, nullable=false)
+     * @ORM\Column(type="string", length=255)
      */
-    private $prefNameKana;
+    private $pref_name_kana;
 
     /**
-     * @var string
-     *
-     * @ORM\Column(name="region_code", type="string", length=255, nullable=false)
+     * @ORM\Column(type="string", length=255)
      */
-    private $regionCode;
+    private $region_code;
 
     /**
-     * @var string
-     *
-     * @ORM\Column(name="region_name", type="string", length=255, nullable=false)
+     * @ORM\Column(type="string", length=255)
      */
-    private $regionName;
+    private $region_name;
 
     /**
-     * @var int
-     *
-     * @ORM\Column(name="area_code", type="integer", nullable=false)
+     * @ORM\Column(type="string", length=255)
      */
-    private $areaCode;
+    private $area_code;
 
     /**
-     * @var string
-     *
-     * @ORM\Column(name="area_name", type="string", length=255, nullable=false)
+     * @ORM\Column(type="string", length=255)
      */
-    private $areaName;
+    private $area_name;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="area_name_kana", type="string", length=255, nullable=false)
-     */
-    private $areaNameKana;
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
 
+    public function getPinpointCode(): ?string
+    {
+        return $this->pinpoint_code;
+    }
 
+    public function setPinpointCode(string $pinpoint_code): self
+    {
+        $this->pinpoint_code = $pinpoint_code;
+
+        return $this;
+    }
+
+    public function getPinpointName(): ?string
+    {
+        return $this->pinpoint_name;
+    }
+
+    public function setPinpointName(string $pinpoint_name): self
+    {
+        $this->pinpoint_name = $pinpoint_name;
+
+        return $this;
+    }
+
+    public function getPinpointNameKana(): ?string
+    {
+        return $this->pinpoint_name_kana;
+    }
+
+    public function setPinpointNameKana(string $pinpoint_name_kana): self
+    {
+        $this->pinpoint_name_kana = $pinpoint_name_kana;
+
+        return $this;
+    }
+
+    public function getPrefCode(): ?string
+    {
+        return $this->pref_code;
+    }
+
+    public function setPrefCode(string $pref_code): self
+    {
+        $this->pref_code = $pref_code;
+
+        return $this;
+    }
+
+    public function getPrefName(): ?string
+    {
+        return $this->pref_name;
+    }
+
+    public function setPrefName(string $pref_name): self
+    {
+        $this->pref_name = $pref_name;
+
+        return $this;
+    }
+
+    public function getPrefNameKana(): ?string
+    {
+        return $this->pref_name_kana;
+    }
+
+    public function setPrefNameKana(string $pref_name_kana): self
+    {
+        $this->pref_name_kana = $pref_name_kana;
+
+        return $this;
+    }
+
+    public function getRegionCode(): ?string
+    {
+        return $this->region_code;
+    }
+
+    public function setRegionCode(string $region_code): self
+    {
+        $this->region_code = $region_code;
+
+        return $this;
+    }
+
+    public function getRegionName(): ?string
+    {
+        return $this->region_name;
+    }
+
+    public function setRegionName(string $region_name): self
+    {
+        $this->region_name = $region_name;
+
+        return $this;
+    }
+
+    public function getAreaCode(): ?string
+    {
+        return $this->area_code;
+    }
+
+    public function setAreaCode(string $area_code): self
+    {
+        $this->area_code = $area_code;
+
+        return $this;
+    }
+
+    public function getAreaName(): ?string
+    {
+        return $this->area_name;
+    }
+
+    public function setAreaName(string $area_name): self
+    {
+        $this->area_name = $area_name;
+
+        return $this;
+    }
 }
