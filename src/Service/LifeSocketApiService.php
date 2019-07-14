82c895598acbf087710a75b0f827c98b3343f845
takashikitamura <monkeydolce@gmail.com>
2019-07-14 15:30:41 +0900

    Add:APIからピンポイント天気情報を取得する

--- src/Service/LifeSocketApiService.php
+++ src/Service/LifeSocketApiService.php
@@ -2,13 +2,14 @@
 
     namespace App\Service;
 
+    use App\Entity\Weather;
     use Symfony\Component\HttpClient\HttpClient;
 
     /**
      * Class LifeSocketApi
-     * @package Service
+     * @package App\Service
      */
-    class LifeSocketApiService
+    final class LifeSocketApiService
     {
 
         /**
@@ -34,10 +35,74 @@
         public function __construct(string $apiKey = '', HttpClient $httpClient = null)
         {
             $this->apiKey     = $apiKey;
-            $this->httpclient = $httpClient ?:HttpClient::create(
-                [
-                    'base_uri' => self::API_BASE_URL
-                ]
-            );
+            $this->httpClient = $httpClient ?: HttpClient::create();
         }
+
+
+        /**
+         * @param string $pinpointCode
+         * @param int $days
+         * @param string $lang
+         *
+         * @throws \Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface
+         * @throws \Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface
+         * @throws \Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface
+         * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
+         */
+        public function getWeatherReportFromPinpoint(
+            string $pinpointCode,
+            int $days = 1,
+            string $lang = 'ja'
+        ): void {
+            $query = array_merge([
+                'days' => $days,
+                'lang' => $lang
+            ]);
+
+
+            $apiUrl = $this->getApiUrl('/v1/weather/') . $pinpointCode;
+
+            $response = $this->getApiData($apiUrl, $query);
+            dump($response);
+
+
+//            return new Weather();
+        }
+
+        /**
+         * 取得したいAPIのURlを生成する
+         *
+         * @param string $apiUrl
+         *
+         * @return string
+         */
+        public function getApiUrl(string $apiUrl): string
+        {
+            return self::API_BASE_URL . $apiUrl;
+        }
+
+
+        /**
+         * @param string $apiUrl
+         * @param array $query
+         *
+         * @return mixed
+         * @throws \Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface
+         * @throws \Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface
+         * @throws \Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface
+         * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
+         */
+        public function getApiData(string $apiUrl, array $query)
+        {
+            $response = $this->httpClient->request('GET', $apiUrl,
+                [
+                    'headers' => ['x-access-key' => $this->apiKey],
+                    'query'   => $query
+                ]
+            )->getContent();
+
+            return json_decode($response, false);
+        }
+
+
     }
---
+++ src/Service/WeatherService.php
@@ -0,0 +1,15 @@
+<?php
+
+    namespace App\Service;
+
+    use App\Entity\Weather;
+    use Symfony\Component\HttpClient\HttpClient;
+
+    /**
+     * Class WeatherService
+     * @package App\Service
+     */
+    final class WeatherService
+    {
+        
+    }
