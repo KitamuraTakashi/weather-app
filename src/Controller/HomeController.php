f8b5da3828da014bb47d98fdce2fbd01cd95590e
takashikitamura <monkeydolce@gmail.com>
2019-07-16 21:57:34 +0900

    Add:DBへの書き込みをする
    SymfonyでDBへの書き込みを確認するために、静的データで挿入
    
    次は取得したデータを挿入する

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
     namespace App\Controller;
 
+    use App\Entity\Weather;
     use App\Service\LifeSocketApiService;
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
     use Symfony\Component\HttpClient\HttpClient;
@@ -14,10 +15,29 @@
          */
         public function index()
         {
-            $response = new LifeSocketApiService('63b05a9d-82ef-4084-8c91-68fae321dafa');
+            $httpClient = new LifeSocketApiService('63b05a9d-82ef-4084-8c91-68fae321dafa');
+//            $response   = $httpClient->getWeatherReportFromPinpoint('1133612');
+
+
+                $weather = new Weather();
+                $weather->setPinpointCode('1133612');
+                $weather->setDataTime('2019');
+                $weather->setWeatherCode('1919');
+                $weather->setWeatherName('晴れ');
+                $weather->setTemperatureMin('10');
+                $weather->setTemperatureMax('20');
+                $weather->setRainPercentage('40');
+
+                $em = $this->getDoctrine()->getManager();
+
+                $em->persist($weather);
+                $em->flush();
+
+
 
             return $this->render('home/index.html.twig', [
                 'controller_name' => 'HomeController',
             ]);
         }
     }
+
